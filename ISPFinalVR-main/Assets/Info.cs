57e612c512d8561803e58227002632d5e74dcaf2
markofin0 <m.marullo25@ncf.edu>
2024-01-31 23:32:06 -0500

    Sources Cited + video added
    Sources Cited + video added

--- ISPFinalVR-main/Assets/Info.cs
+++ ISPFinalVR-main/Assets/Info.cs
@@ -1,6 +1,6 @@
 /*
  * 
- * https://youtu.be/CV1FCWqBcPo
+ * https://youtu.be/l3CwEMPqvMs
  * 
  * 
  * https://stackoverflow.com/questions/38898035/unity-gameobjects-with-tag-reset-to-start-location-position
@@ -9,6 +9,9 @@ https://discussions.unity.com/t/i-want-to-reset-my-object-to-its-original-positi
 
 https://gamedevbeginner.com/how-to-move-objects-in-unity/#move_object
 https://www.youtube.com/watch?v=DX7HyN7oJjE
+https://stackoverflow.com/questions/61478642/unity-shift-to-sprint
+https://gamedevbeginner.com/singletons-in-unity-the-right-way/
+https://stackoverflow.com/questions/13730112/unity-singleton-manager-classes
 
 /*
  * Mark
@@ -16,18 +19,23 @@ https://www.youtube.com/watch?v=DX7HyN7oJjE
  * UI for score tracking
  * button on landing page
  * bugfixing
- * recording
+ * General layout of levels
+ * Github implementation
+ * Game build
+ * Scene 3
  * Ryan
  * collisions with buses
  * arrow key movements
  * triggering
  * tagging vehicles
+ * Scene 1
  * Michael
  * character behavior
  * carbehavior
  * mover scripts for each 
  * reset spawn on collision with car
- * 
+ * Audio / video recordings
+ * Scene 2
  * 
  * 
  */
